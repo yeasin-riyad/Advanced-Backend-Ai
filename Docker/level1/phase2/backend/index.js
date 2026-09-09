61e6904febbe6ae68f80d7d3c0c7cb1645ef131f
Yeasin-Riyad <mazumderyeasin98@gmail.com>
2026-09-09 19:04:26 +0600

    Add package.json for backend with initial dependencies

---
+++ Docker/level1/phase2/backend/index.js
@@ -0,0 +1,15 @@
+import express from "express"
+import dotenv from "dotenv"
+dotenv.config()
+
+const port=process.env.PORT || 5000
+
+const app=express()
+
+app.get("/",(req,res)=>{
+   return res.status(200).json({message:"hello from docker phase 2"})
+})
+
+app.listen(port,()=>{
+    console.log(`server started ${port}`)
+})
