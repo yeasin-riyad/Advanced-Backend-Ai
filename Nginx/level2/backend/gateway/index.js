555992474f7d3b9692f76ad23b6c2f754a56e791
Yeasin-Riyad <mazumderyeasin98@gmail.com>
2026-09-14 00:13:00 +0600

    feat: set up Nginx configuration and Docker setup for gateway and services

--- Nginx/level2/backend/gateway/index.js
+++ Nginx/level2/backend/gateway/index.js
@@ -7,10 +7,14 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.get("/",(req,res)=>{
+    return res.status(200).json({message:`hello from ${process.env.SERVER_NAME}`})
+})
+
 app.use(express.json());
-app.use("/auth",proxy("http://localhost:8001"))
-app.use("/order",proxy("http://localhost:8002"))
-app.use("/product",proxy("http://localhost:8003"))
+app.use("/auth",proxy("http://auth-service:8001"))
+app.use("/order",proxy("http://order-service:8002"))
+app.use("/product",proxy("http://product-service:8003"))
 
 app.listen(port, () => {
   console.log(`server started ${port}`);
